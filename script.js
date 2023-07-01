498192d6cf219ea8a0d7a9383fdacc7f9f44f9bc
LanchuNaik <134943642+LanchuNaik@users.noreply.github.com>
2023-07-01 11:37:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var tablink=document.getElementsByClassName("tab-link");
+var tabcontent=document.getElementsByClassName("tab-content");
+   
+function opentab(tabname){
+  for(tablinks of tablink){
+     tablinks.classList.remove("active-link");
+  }
+  for(tabcontents of tabcontent){
+    tabcontents.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+ var sidemenu=document.getElementById("sidemenu");
+ function openmenu(){
+  sidemenu.style.right="0";
+ }
+ function closemenu(){
+  sidemenu.style.right="-200px";
+ }
